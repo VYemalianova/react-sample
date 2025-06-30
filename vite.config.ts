bd8b3766a4893a83c293df98b3c65ac96010833e
Viktoriia Yemelianova <v.yemelianova@mobidev.biz>
2025-06-30 20:19:18 +0300

    add global styles

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+          @use "styles/variables.scss" as variables;
+          @use "styles/color-classes.scss" as colors;
+          @use "styles/global.scss" as *;
+        `,
+      },
+    },
+  },
 });
