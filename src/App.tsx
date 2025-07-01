e20a941d4374c9fc3508214aa683b4b14ee4213e
Viktoriia Yemelianova <v.yemelianova@mobidev.biz>
2025-07-01 15:39:40 +0300

    add material theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
+import { ThemeProvider } from '@mui/material';
+
+import './styles/global.scss';
 import './App.scss';
+
+import { MuiTheme } from './theme';
 
 function App() {
   return (
-    <div className="page-layout">
-      <div className="header mt-s mb-s pr-xl bg-black">header</div>
-      <div className="body">body</div>
-      <div className="footer">footer</div>
-    </div>
+    <ThemeProvider theme={MuiTheme}>
+      <div className="page-layout">
+        <div className="header mt-s mb-s pr-xl bg-black">header</div>
+        <div className="body">body</div>
+        <div className="footer">footer</div>
+      </div>
+    </ThemeProvider>
   );
 }
 
