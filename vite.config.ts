ea1849bea0ff548cd84f59c70f89ca3d9595b8fb
Viktoriia Yemelianova <v.yemelianova@mobidev.biz>
2025-07-08 13:13:23 +0300

    add global styles

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,23 @@ import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+          @use "@styles/global.scss" as *;
+          @use "@styles/variables.scss" as variables;
+          @use "@styles/color-classes.scss" as colors;
+        `,
+      },
+    },
+  },
   resolve: {
     alias: {
       '@assets': path.resolve(__dirname, 'src/assets'),
       '@components': path.resolve(__dirname, 'src/components'),
       '@pages': path.resolve(__dirname, 'src/pages'),
+      '@styles': path.resolve(__dirname, 'src/styles'),
     },
   },
 });
