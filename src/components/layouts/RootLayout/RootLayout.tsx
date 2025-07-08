5e047c9feb291071f9f3ab39591f632a7835f4c3
Viktoriia Yemelianova <v.yemelianova@mobidev.biz>
2025-07-08 14:37:13 +0300

    add material theme

--- src/components/layouts/RootLayout/RootLayout.tsx
+++ src/components/layouts/RootLayout/RootLayout.tsx
@@ -1,15 +1,21 @@
+import { ThemeProvider } from '@mui/material';
 import { Outlet } from 'react-router-dom';
+
 import styles from './RootLayout.module.scss';
+
+import { MuiTheme } from '../../../theme';
 
 const Layout = () => {
   return (
-    <div className={styles['page-layout']}>
-      <header>Header</header>
-      <main className={styles['page-content']}>
-        <Outlet />
-      </main>
-      <footer>Footer</footer>
-    </div>
+    <ThemeProvider theme={MuiTheme}>
+      <div className={styles['page-layout']}>
+        <header>Header</header>
+        <main className={styles['page-content']}>
+          <Outlet />
+        </main>
+        <footer>Footer</footer>
+      </div>
+    </ThemeProvider>
   );
 };
 
---
+++ src/theme.ts
@@ -0,0 +1,74 @@
+import { createTheme } from '@mui/material';
+
+declare module '@mui/material/Button' {
+  interface ButtonPropsVariantOverrides {
+    light: true;
+    dark: true;
+    gradient: true;
+  }
+}
+
+export const MuiTheme = createTheme({
+  components: {
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          color: '#ffffff',
+          fontSize: '16px',
+          lineHeight: '24px',
+          textTransform: 'none',
+          borderRadius: '32px',
+          '&:hover': {
+            boxShadow:
+              'rgba(50, 50, 93, 0.25) 0px 30px 60px -12px inset, rgba(0, 0, 0, 0.3) 0px 18px 36px -18px inset',
+          },
+          variants: [
+            {
+              props: { variant: 'light' },
+              style: {
+                color: '#630CD2',
+                backgroundColor: '#E4D1F5',
+                '&:hover': {
+                  boxShadow:
+                    'rgba(50, 50, 93, 0.12) 0px 30px 60px -12px inset, rgba(0, 0, 0, 0.15) 0px 18px 36px -18px inset;',
+                },
+              },
+            },
+            {
+              props: { variant: 'dark' },
+              style: {
+                backgroundColor: '#630CD2',
+              },
+            },
+            {
+              props: { variant: 'gradient' },
+              style: {
+                backgroundImage: 'linear-gradient(45deg, #630cd2, #f95f86)',
+              },
+            },
+            {
+              props: { variant: 'text' },
+              style: {
+                color: '#08030e',
+                '&:hover': {
+                  color: '#630cd2',
+                  boxShadow: 'none',
+                  outline: 'none',
+                  backgroundColor: 'transparent',
+                },
+              },
+            },
+          ],
+        },
+      },
+    },
+    MuiList: {
+      styleOverrides: {
+        root: {
+          paddingTop: '0px',
+          paddingBottom: '0px',
+        },
+      },
+    },
+  },
+});
