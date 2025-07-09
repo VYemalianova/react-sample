7ff7d0493fa2ca357e00882385e43a80b50c70b3
Viktoriia Yemelianova <v.yemelianova@mobidev.biz>
2025-07-09 10:22:42 +0300

    add header

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,28 @@
+import { Link } from 'react-router-dom';
+import { Button } from '@mui/material';
+import FlareIcon from '@mui/icons-material/Flare';
+
+import styles from './Header.module.scss';
+import lockImg from '@assets/lock.svg';
+
+const Header = () => {
+  return (
+    <header className={`${styles.header} gradient--moonraker-remy`}>
+      <div className={styles['header-content']}>
+        <Link to="/" className={`${styles['logo-wrapper']} link`}>
+          <img src={lockImg} />
+          <span className="gradient--text uppercase">AstroYod</span>
+        </Link>
+
+        <div className={styles['btns-wrapper']}>
+          <Button variant="dark">Sign Up</Button>
+          <Button variant="gradient" endIcon={<FlareIcon />}>
+            Live Psychics
+          </Button>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
--- src/components/layouts/RootLayout/RootLayout.tsx
+++ src/components/layouts/RootLayout/RootLayout.tsx
@@ -1,15 +1,16 @@
 import { ThemeProvider } from '@mui/material';
 import { Outlet } from 'react-router-dom';
 
-import styles from './RootLayout.module.scss';
+import Header from '@components/Header/Header';
 
+import styles from './RootLayout.module.scss';
 import { MuiTheme } from '../../../theme';
 
 const Layout = () => {
   return (
     <ThemeProvider theme={MuiTheme}>
       <div className={styles['page-layout']}>
-        <header>Header</header>
+        <Header />
         <main className={styles['page-content']}>
           <Outlet />
         </main>
