ec7cb8f679a6ea919f6a0148daf3a0b35db798f2
Viktoriia Yemelianova <v.yemelianova@mobidev.biz>
2025-07-22 18:44:34 +0300

    add banner component

--- src/pages/HoroscopesPage/HoroscopesPage.tsx
+++ src/pages/HoroscopesPage/HoroscopesPage.tsx
@@ -1,5 +1,16 @@
-const DailyHoroscope = () => {
-  return <div>Daily Horoscope</div>;
+import { useParams } from 'react-router-dom';
+
+import type { HoroscopeType } from '../../models/horoscope';
+import HoroscopeBanner from '../../components/HoroscopeBanner/HoroscopeBanner';
+
+const Horoscopes = () => {
+  const { type } = useParams<{ type: HoroscopeType }>();
+
+  return (
+    <div>
+      <HoroscopeBanner type={type!} />
+    </div>
+  );
 };
 
-export default DailyHoroscope;
+export default Horoscopes;
--- src/utils/dateUtils.ts
+++ src/utils/dateUtils.ts
@@ -1,27 +1,12 @@
 import dayjs from 'dayjs';
 import type { IDatePart } from '../models/datePart';
 
-export enum DateFormat {
-  short = 'short',
-  long = 'long',
-}
-
-export const formatDate = (date: Date, format = DateFormat.short): string => {
-  if (isNaN(date.getTime())) {
-    return 'Invalid date';
-  }
-
-  const options: Intl.DateTimeFormatOptions = { month: 'short', day: 'numeric' };
-
-  if (format === DateFormat.long) {
-    options.year = 'numeric';
-  }
-
-  return new Intl.DateTimeFormat('en-US', options).format(date);
+export const getWeekday = (date: Date) => {
+  return dayjs(date).format('dddd');
 };
 
-export const getWeekday = (date: Date) => {
-  return new Intl.DateTimeFormat('en-US', { weekday: 'long' }).format(date);
+export const formatDate = (format = 'MMM D') => {
+  return dayjs().format(format);
 };
 
 export const formatDateFromParts = (dateParts: IDatePart, format = 'MMM D') => {
